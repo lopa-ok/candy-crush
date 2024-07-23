9cdc9eb996a5422a7b11689e260775f6f126c57b
philopater <lopa@programmer.net>
2024-07-23 22:22:44 +0300

    first

---
+++ app.js
@@ -0,0 +1,110 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.getElementById('grid');
+    const width = 8;
+    const squares = [];
+    const candyColors = ['red', 'yellow', 'blue', 'green', 'orange', 'purple'];
+
+
+    function createBoard() {
+        for (let i = 0; i < width * width; i++) {
+            const square = document.createElement('div');
+            const randomColor = Math.floor(Math.random() * candyColors.length);
+            square.classList.add('candy');
+            square.classList.add(candyColors[randomColor]);
+            square.setAttribute('draggable', true);
+            square.setAttribute('id', i);
+            grid.appendChild(square);
+            squares.push(square);
+        }
+    }
+
+    createBoard();
+
+    let colorBeingDragged;
+    let colorBeingReplaced;
+    let squareIdBeingDragged;
+    let squareIdBeingReplaced;
+
+    squares.forEach(square => square.addEventListener('dragstart', dragStart));
+    squares.forEach(square => square.addEventListener('dragend', dragEnd));
+    squares.forEach(square => square.addEventListener('dragover', dragOver));
+    squares.forEach(square => square.addEventListener('dragenter', dragEnter));
+    squares.forEach(square => square.addEventListener('dragleave', dragLeave));
+    squares.forEach(square => square.addEventListener('drop', dragDrop));
+
+    function dragStart() {
+        colorBeingDragged = this.className.split(' ')[1];
+        squareIdBeingDragged = parseInt(this.id);
+    }
+
+    function dragOver(e) {
+        e.preventDefault();
+    }
+
+    function dragEnter(e) {
+        e.preventDefault();
+    }
+
+    function dragLeave() {
+        this.style.backgroundColor = '';
+    }
+
+    function dragDrop() {
+        colorBeingReplaced = this.className.split(' ')[1];
+        squareIdBeingReplaced = parseInt(this.id);
+        squares[squareIdBeingDragged].className = 'candy ' + colorBeingReplaced;
+        this.className = 'candy ' + colorBeingDragged;
+    }
+
+    function dragEnd() {
+        let validMoves = [
+            squareIdBeingDragged - 1,
+            squareIdBeingDragged - width,
+            squareIdBeingDragged + 1,
+            squareIdBeingDragged + width
+        ];
+
+        let validMove = validMoves.includes(squareIdBeingReplaced);
+
+        if (squareIdBeingReplaced && validMove) {
+            squareIdBeingReplaced = null;
+        } else if (squareIdBeingReplaced && !validMove) {
+            squares[squareIdBeingReplaced].className = 'candy ' + colorBeingReplaced;
+            squares[squareIdBeingDragged].className = 'candy ' + colorBeingDragged;
+        } else squares[squareIdBeingDragged].className = 'candy ' + colorBeingDragged;
+    }
+
+    // Check for matches
+    function checkRowForThree() {
+        for (let i = 0; i < 61; i++) {
+            let rowOfThree = [i, i + 1, i + 2];
+            let decidedColor = squares[i].className.split(' ')[1];
+            const isBlank = squares[i].classList.contains('blank');
+
+            if (rowOfThree.every(index => squares[index].className.includes(decidedColor) && !isBlank)) {
+                rowOfThree.forEach(index => {
+                    squares[index].className = 'candy blank';
+                });
+            }
+        }
+    }
+
+    function checkColumnForThree() {
+        for (let i = 0; i < 47; i++) {
+            let columnOfThree = [i, i + width, i + width * 2];
+            let decidedColor = squares[i].className.split(' ')[1];
+            const isBlank = squares[i].classList.contains('blank');
+
+            if (columnOfThree.every(index => squares[index].className.includes(decidedColor) && !isBlank)) {
+                columnOfThree.forEach(index => {
+                    squares[index].className = 'candy blank';
+                });
+            }
+        }
+    }
+
+    window.setInterval(function () {
+        checkRowForThree();
+        checkColumnForThree();
+    }, 100);
+});
